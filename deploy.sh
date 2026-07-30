3b2474edadf45b7eb3a488ce929daa98e361e10b
mohamedadlani2007-bit <mohamedadlani2007@gmail.com>
2026-07-30 01:11:10 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,26 @@
+ملف deploy.sh:
 #!/bin/bash
-export BOT_TOKEN="8348188479:AAFMAzyBi5KQzdYEFTEtz1ktnhqnmclat7Q"
+
+# متغيرات
+export BOT_TOKEN="8348183979:AAFMAzyBi5KQzdYEFTEtz1ktnhqnmclat7Q"
 export IMAGE="knhfdsjj/mohamed-alamia:v3"
 export SERVICE_NAME="mohamed-bot"
 export REGION="us-central1"
+
+# تمكين الخدمات
 gcloud services enable run.googleapis.com containerregistry.googleapis.com --quiet
-gcloud run deploy $SERVICE_NAME --image $IMAGE --platform managed --region $REGION --allow-unauthenticated --set-env-vars="BOT_TOKEN=$BOT_TOKEN" --quiet
+
+# النشر مباشرة من الصورة الموجودة
+gcloud run deploy $SERVICE_NAME \
+  --image $IMAGE \
+  --platform managed \
+  --region $REGION \
+  --allow-unauthenticated \
+  --set-env-vars="BOT_TOKEN=$BOT_TOKEN" \
+  --quiet
+
+# عرض رابط الخدمة
 echo "✅ تم النشر!"
-gcloud run services describe $SERVICE_NAME --region $REGION --format="value(status.url)"
+gcloud run services describe $SERVICE_NAME \
+  --region $REGION \
+  --format="value(status.url)"
